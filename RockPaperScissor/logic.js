9efbd42b954384f9e81ac9eac5226d0edf993721
Harman Randhawa <54494701+randhawah@users.noreply.github.com>
2022-06-13 20:15:19 -0400

    Fix errors and login in Rock Paper Scissor
    program would always result in a draw, fix the logic by returning
    the result of game() as an array, so we can have all 3 values of wins,
    looses and draws and compare them to determine the overall winner from
    5 rounds. Program is now working fine. :)

--- RockPaperScissor/logic.js
+++ RockPaperScissor/logic.js
@@ -11,58 +11,90 @@ function computerPlay(){
 function playRound(player, computer){
     console.log("Player choice : " + player);
     console.log("Computer choice : " + computer);
-    let result = "Its a DRAW!!";
-    let wins = 0;
+    let result = "";
+    let user = 0;
+    let cpu = 0;
+    let draw = 0;
 
     if(player == 'rock' && computer == 'scissor'){
         result = "Rock beats Scissors, you WIN!!!";
-        wins++;
+        user++;
     }
 
     else if(player == 'rock' && computer == 'paper'){
         result = "Paper beats Rock, you LOOSE!!";  
+        cpu++;
     }
 
     else if(player == 'paper' && computer == 'rock'){
         result = "Paper beats Rock, you WIN!!";
-        wins++;
+        user++;
     }
 
     else if(player == 'paper' && computer == 'scissor'){
         result = "Scissors beats Paper, you LOOSE!!";
+        cpu++;
     }
 
      else if(player == 'scissor' && computer == 'paper'){
         result = "Scissors beats Paper, you Win!!";
-        wins++;
+        user++;
     }
 
     else if(player == 'scissor' && computer == 'rock'){
         result = "Rock beats Scissor, you LOOSE!!";
+        cpu++;
+    }
+    else{
+        result = "Its a DRAW!!";
+        draw++;
     }
 
         console.log(result);
-        return wins;
+        return arrayFromArgs(user, cpu, draw);
 }
 
+
+function arrayFromArgs() {
+    var results = [];
+    for (var i = 0; i < arguments.length; i++) {
+        results.push(arguments[i]);
+    }
+    return results;
+}
+
+
 function game(){
-let score = 0;
+//let score = new Array();
+let wins = 0;
+let looses = 0;
+let draws = 0;
     for(let i = 0; i<5; i++){
-        playerSelection = prompt("Please enter your choice", "Rock / Paper / Scissor").toLowerCase();
-        const computerSelection = computerPlay();
-        score += playRound(playerSelection, computerSelection);
+        let playerSelection = prompt("Please enter your choice", "Rock / Paper / Scissor").toLowerCase();
+        let computerSelection = computerPlay();
+        let score = playRound(playerSelection, computerSelection);
+        wins += score[0];
+        looses += score[1];
+        draws += score[2];
         console.log(" ");
     }
-    declareWinner(score);
-}
-function declareWinner(score){
-    if(score >= 3){
+
+    console.log("Wins : " + wins);
+    console.log("Lost : " + looses);
+    console.log("Draws : " + draws);
+
+
+    if(wins>looses){
         console.log("");
         console.log("Congratulations, you WIN!!");
     }
-    else{
+    else if(wins<looses){
         console.log("Sorry you LOOSE!!");
     }
+    else{
+        console.log("It was a DRAW!!");
+    }
+
 }
     
 game();
